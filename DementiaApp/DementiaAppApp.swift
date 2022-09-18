dd98b5eeb778defda7c008cddd79b4d14e8b16cd
faisalhussaini <64606546+faisalhussaini@users.noreply.github.com>
2022-09-18 13:13:08 -0400

    Initial Commit

---
+++ DementiaApp/DementiaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DementiaAppApp.swift
+//  DementiaApp
+//
+//  Created by Faisal Hussaini on 2022-09-18.
+//
+
+import SwiftUI
+
+@main
+struct DementiaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
